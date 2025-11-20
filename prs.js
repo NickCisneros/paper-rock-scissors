fa6bdf0ff7ab0190d0b63ab822073e1997794726
NickCisneros <221739789+NickCisneros@users.noreply.github.com>
2025-11-20 12:18:31 -0500

    Create the code for getHumanChoice.

--- prs.js
+++ prs.js
@@ -1,18 +1,26 @@
-
-
-
 function getComputerChoice() {
     
-let a = Math.random()
+    let a = Math.random()
 
-if (a >= .66) {
-return "Paper"
-} else if (a >= .33) {
-return "Rock"
-} else if (a >= 0) {
-return "Scissors"
+    if (a >= .66) {
+        return "Paper"
+    } else if (a >= .33) {
+        return "Rock"
+    } else if (a >= 0) {
+        return "Scissors"
+    }
 }
 
-}
+function getHumanChoice() {
+    let answer = prompt("Rock, Paper, or Scissors?")
 
-console.log(getComputerChoice())
+    if (answer === null) {
+        return "Not this time, Gamer!"
+    } else if (answer.toLowerCase() === "rock") {
+        return "Rock"
+    } else if (answer.toLowerCase() === "paper") {
+        return "Paper"
+    } else if (answer.toLowerCase() === "scissors") {
+        return "Scissors"
+    }
+}
