db314a2a5f09cf9640924a9a450c493b808476d1
NickCisneros <221739789+NickCisneros@users.noreply.github.com>
2025-11-22 15:53:49 -0500

    Add winning logic, step 5.

--- prs.js
+++ prs.js
@@ -3,27 +3,54 @@ function getComputerChoice() {
     let a = Math.random()
 
     if (a >= .66) {
-        return "Paper"
+        return "paper"
     } else if (a >= .33) {
-        return "Rock"
+        return "rock"
     } else if (a >= 0) {
-        return "Scissors"
+        return "scissors"
     }
 }
 
 function getHumanChoice() {
     let answer = prompt("rock, paper, or scissors?")
 
-    if (answer === null) {
-        return "Come on, Gamer!"
-    } else if (answer === "rock") {
-        return "Rock"
+    if (answer === "rock") {
+        return "rock"
     } else if (answer === "paper") {
-        return "Paper"
+        return "paper"
     } else if (answer === "scissors") {
-        return "Scissors"
+        return "scissors"
+    } else { 
+        return "I failed!" 
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playground(humanChoice, computerChoice) {
+    if (humanSelection === computerSelection) {
+        return "Tie!"
+    } else if ((humanSelection === "rock") && (computerSelection === "paper")) {
+        return "Computer wins!"
+    } else if ((humanSelection === "paper") && (computerSelection === "scissors")) {
+        return "Computer wins!"
+    } else if ((humanSelection === "scissors") && (computerSelection === "rock")) {
+        return "Computer wins!"
+    } else if ((humanSelection === "rock") && (computerSelection === "scissors")) {
+        return "Human wins!"
+    } else if ((humanSelection === "paper") && (computerSelection === "rock")) {
+        return "Human wins!"
+    } else if ((humanSelection === "scissors") && (computerSelection === "paper")) {
+        return "Human wins!"
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playground(humanSelection, computerSelection))
+
+
+
+
