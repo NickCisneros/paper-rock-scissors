dcbc6cab2ac3de337fb1479a96248a88bb4ac049
NickCisneros <221739789+NickCisneros@users.noreply.github.com>
2025-11-26 10:47:46 -0500

    Add spare code doc?

---
+++ sparecode.js
@@ -0,0 +1,25 @@
+/*
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie!");
+    } else if ((humanChoice === "rock") && (computerChoice === "paper")) {
+        computerScore++;
+        console.log("Rock loses to paper! Computer wins!");
+    } else if ((humanChoice === "paper") && (computerChoice === "scissors")) {
+        computerScore++;
+        console.log("Paper loses to scissors! Computer wins!");
+    } else if ((humanChoice === "scissors") && (computerChoice === "rock")) {
+        computerScore++;
+        console.log("Scissors loses to rock! Computer wins!");
+    } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
+        humanScore++;
+        console.log("Rock beats scissors! Human wins!");
+    } else if ((humanChoice === "paper") && (computerChoice === "rock")) {
+        humanScore++;
+        console.log("Paper beats rock! Human wins!");
+    } else if ((humanChoice === "scissors") && (computerChoice === "paper")) {
+        humanScore++;
+        console.log("Scissors beats paper! Human wins!");
+    }
+}
+*/
